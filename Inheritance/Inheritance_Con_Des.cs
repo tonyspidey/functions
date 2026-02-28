76349e4aed23ee8cf8dfdd87e19fbd9127d9e982
tonyspidey <harshavn0402@gmail.com>
2026-02-28 15:28:31 +0530

    reference material

---
+++ Inheritance/Inheritance_Con_Des.cs
@@ -0,0 +1,47 @@
+﻿//TWO CLASSES ACCOUNT AND SAVINGS ACCOUNT USING CONSTRUCTOR ASSIGN THE VALUE AND DISPLAY 
+
+using System;
+class Account
+{
+     int accountNumber;
+
+    public Account(int acc)
+    {
+        accountNumber = acc;
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Account Number :"+ accountNumber);
+    }
+
+    ~Account()
+    {
+        Console.WriteLine("Destructor for Account class");
+    }
+}
+
+class SavingsAccount : Account
+{
+     double balance;
+
+    public SavingsAccount(int acc, double bal) : base(acc)
+    {
+        balance = bal;
+    }
+
+    public void Display()
+    {
+        DisplayDetails();
+        Console.WriteLine("Balance: " + balance);
+    }
+}
+
+class Inheritance_Con_Des
+{
+    static void Main(String[] args)
+    {
+        SavingsAccount obj = new SavingsAccount(12345, 50000);
+        obj.Display();
+    }
+}
