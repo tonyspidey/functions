3ac055a4827f2b7c420ad30b7a5a7e9f6c3a633f
tonyspidey <harshavn0402@gmail.com>
2026-02-28 15:28:41 +0530

    reference material

---
+++ Inheritance/MultilevelInheritance.cs
@@ -0,0 +1,63 @@
+﻿//THERE SHOULD BE THREE CLASSES PERSON,STUDENT,GRADUATESTUDENT. STUDENT WILL INHERIT THE PERSON CLASS AND GRADUATESTUDENT STUDENT WILL INHERIT THE STUDENT CLASS.
+//GET NAME , ROLL NUMBER AND PROJECT TITLE IN EACH CLASSES AND DISPLAY ALL.
+
+using System;
+class Person
+{
+     string Name;
+
+    public void GetName()
+    {
+        Console.Write("Enter Name: ");
+        Name = Console.ReadLine();
+    }
+    public void DisplayName()
+    {
+            Console.WriteLine("Name: " + Name);
+     }
+}
+
+class Student : Person
+{
+     int RollNumber;
+
+    public void GetRollNumber()
+    {
+        Console.Write("Enter Roll Number: ");
+        RollNumber = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void DisplayRollNumber()
+    {
+        Console.WriteLine("Roll number: " + RollNumber);
+    }
+}
+
+class GraduateStudent : Student
+{
+    public string ProjectTitle;
+
+    public void GetProjectTitle()
+        {
+            Console.Write("Enter Project Title: ");
+            ProjectTitle = Console.ReadLine();
+    }
+    public void DisplayProfile()
+    {
+        DisplayName();
+        DisplayRollNumber();
+        Console.WriteLine("Project title: " + ProjectTitle);
+    }
+}
+
+class MultilevelInheritance
+{
+    static void Main()
+    {
+        GraduateStudent obj = new GraduateStudent();
+        obj.GetName();
+        obj.GetRollNumber();
+        obj.GetProjectTitle();
+        obj.DisplayProfile();
+    }
+}
---
+++ Inheritance/SingleInheritance.cs
@@ -0,0 +1,50 @@
+﻿//GET EMPLOYEE ID ,NAME ,MONTHLY SALARY AND CALCULATE ANNUAL SALARY
+
+using System;
+class Employee
+{
+     int empId;
+     string name;
+
+    public void GetDetails()
+    {
+        Console.Write(" Employee ID: ");
+        empId = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Name: ");
+        name = Console.ReadLine();
+    }
+    public void DisplayDetails()
+    {
+        Console.WriteLine("ID: " + empId);
+        Console.WriteLine("Name: " + name);
+    }
+}
+
+class FullTimeEmployee : Employee
+{
+     double monthlySalary;
+    public void GetSalaryDetails()
+    {
+        Console.Write("Enter Monthly Salary: ");
+        monthlySalary = Convert.ToDouble(Console.ReadLine());
+    }
+    public void CalculateAnnualSalary()
+    {
+        double annualSalary = monthlySalary * 12;   
+        
+        Console.WriteLine("Annual Salary: " + annualSalary);
+    }
+}
+
+class SingleInheritance
+{
+    static void Main(String[] args)
+    {
+        FullTimeEmployee obj = new FullTimeEmployee();
+        obj.GetDetails();
+        obj.GetSalaryDetails();
+        obj.CalculateAnnualSalary();
+        obj.DisplayDetails();
+    }
+}
