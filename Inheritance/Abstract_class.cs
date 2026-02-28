3f32055c0630f07514528f5aa95f8a5bf258d124
tonyspidey <harshavn0402@gmail.com>
2026-02-28 15:27:48 +0530

    new file

---
+++ Inheritance/Abstract_class.cs
@@ -0,0 +1,43 @@
+﻿//ABSTRACT PROGRAM 
+
+using System;
+abstract class Vehicle
+{
+    public abstract void Start();
+    public void Stop()
+    {
+        Console.WriteLine("Stop the Vehicle");
+    }
+}
+
+class Car: Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("Start the Car");
+    }
+}
+
+class Bike: Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("Start the Bike");
+    }
+}
+
+class Abstract_class
+{
+    static void Main(string[] args)
+    {
+        Car obj = new Car();
+
+        Bike obj2 = new Bike();
+
+        obj.Start();
+        obj.Stop();
+
+        obj2.Start();
+        obj2.Stop();
+    }
+}
