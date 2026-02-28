0400e2b07622684d952c683b5022d5c967459c1e
tonyspidey <harshavn0402@gmail.com>
2026-02-28 15:28:18 +0530

    reference material

---
+++ Inheritance/Base_keyword_pgm.cs
@@ -0,0 +1,42 @@
+﻿//WITH BASE KEYWORD CALL THE METHOD 
+
+using System;
+class Company
+{
+     string companyName;
+
+    public Company(string name)
+    {
+        companyName = name;
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Company: " + companyName);
+    }
+}
+
+class Department : Company
+{
+    string departmentName;
+
+    public Department(string cname, string dname) : base(cname)
+    {
+        departmentName = dname;
+    }
+
+    public void Display()
+    {
+        base.DisplayDetails();  
+        Console.WriteLine("Department: " + departmentName);
+    }
+}
+
+class Base_Keyword_pgm
+{
+    static void Main()
+    {
+        Department d = new Department("CG - VAK ", "Developer");
+        d.Display();
+    }
+}
